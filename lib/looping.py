b17b387561b29742fe282fee8ade8b96fc53f784
Brett de Bear <bkdebear@gmail.com>
2023-02-21 19:41:45 -0800

    did it

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+   counter = 10
+   while counter > 0:
+       print(counter)
+       counter = counter - 1
+   print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_list = [number * number for number in int_list]
+    return square_list
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(100):
+        i = i + 1
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 5 == 0:
+            print("Buzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        else:
+            print(i)
